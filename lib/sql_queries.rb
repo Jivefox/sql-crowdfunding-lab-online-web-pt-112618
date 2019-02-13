4c1bbe70062dacdcf6486ba94a75cf4744f849ea
Cliff Jurgensen <jivefox.002@gmail.com>
2019-02-13 05:03:16 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -45,8 +45,8 @@ end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
 "SELECT projects.category, SUM(pledges.amount)
-FROM projects
-JOIN pledges
+FROM pledges
+JOIN projects
 ON pledges.project_id = projects.id
 WHERE projects.category = 'books'"
 end
